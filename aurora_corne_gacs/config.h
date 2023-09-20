72fc10758f2391ec9a2bd49601270852858a9c80
Guillaume Debros <guillaume.debros@digeiz.com>
2023-09-20 16:12:34 +0200

    tapping options in config.h

--- aurora_corne_gacs/config.h
+++ aurora_corne_gacs/config.h
@@ -5,7 +5,6 @@
 // will perform its tap action. If the HOLD_ON_OTHER_KEY_PRESS option is
 // enabled, the dual-role key will perform its hold action instead.
 #define HOLD_ON_OTHER_KEY_PRESS
-#define HOLD_ON_OTHER_KEY_PRESS
 // When enabled, typing a mod-tap plus second within term will register as the mod-combo
 // Ref: https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold 
 #define PERMISSIVE_HOLD
