8dc791b8597578a015923e3a8de48b88c56e6596
Guillaume Debros <guillaume.debros@digeiz.com>
2023-09-20 09:24:20 +0200

    added gacs home row keymap

---
+++ aurora_corne_gacs/config.h
@@ -0,0 +1,2 @@
+#define TAPPING_TERM 175
+#define QUICK_TAP_TERM 0
---
+++ aurora_corne_gacs/rules.mk
@@ -0,0 +1,5 @@
+#include QMK_KEYBOARD_H
+
+TAP_DANCE_ENABLE = yes
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+WPM_ENABLE = yes
