0bbe7e9f490ceac5cde2d8c2e98c98aeeb75d13d
Guillaume Debros <guillaume.debros@digeiz.com>
2023-09-20 12:46:38 +0200

    fixed NAV layout

--- aurora_corne_gacs/keymap.c
+++ aurora_corne_gacs/keymap.c
@@ -6,6 +6,7 @@
 #define ALT_S LALT_T(KC_S)
 #define SFT_D LSFT_T(KC_D)
 #define CTL_F LCTL_T(KC_F)
+#define SFT_DN LSFT_T(KC_PGDN)
 
 // Right-hand home row mods
 #define CTL_J RCTL_T(KC_J)
@@ -44,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NAV] = LAYOUT_split_3x6_3(
 
       KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                       KC_6, KC_7, KC_8, KC_9, KC_0, KC_DELETE,
-      KC_PGUP, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,       KC_APP, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
-      KC_PGDN, XXXXXXX, KC_CUT, KC_COPY, KC_PAST, LCTL(KC_B),    KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXXXXX, LCTL(LALT(KC_DEL)),
+      KC_PGUP, KC_P1, KC_P2, KC_P3, KC_P4, KC_P5,                 KC_P6, KC_P7, KC_P8, KC_P9, KC_P0, KC_APP,
+      SFT_DN, XXXXXXX, KC_CUT, KC_COPY, KC_PAST, LCTL(KC_B),      KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_COMM, KC_DOT,
                     _______, _______, _______,                    _______, MO(_CONF), _______
 
                     ),
@@ -55,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
+      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
                     _______, MO(_CONF), _______,                      _______, _______, _______
 
                     ),
@@ -63,10 +64,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_CONF] = LAYOUT_split_3x6_3(
 
-      QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       TG(_BASE), TG(_NAV), TG(_SYMB), TG(_CONF), TG(_NUM), TG(_FUNC),
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,       TG(_EMPTY), TG(7), TG(8), TG(9), TG(10), TG(11),
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,       TG(12), TG(13), TG(14), TG(15), XXXXXXX, XXXXXXX,
-                    _______, _______, _______,                    _______, _______, _______
+      QK_BOOT, LCTL(LALT(KC_DEL)), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       TG(_BASE), TG(_NAV), TG(_SYMB), TG(_CONF), TG(_NUM), TG(_FUNC),
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                  TG(_EMPTY), TG(7), TG(8), TG(9), TG(10), TG(11),
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                  TG(12), TG(13), TG(14), TG(15), XXXXXXX, XXXXXXX,
+                    _______, _______, _______,                               _______, _______, _______
 
                     ),
 
